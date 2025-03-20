0da469327a6f257613ca361a39bf31dda69dd03a
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2025-03-20 11:16:56 +0100

    pydantic task

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pydantic Airflow provider
+
+Exposes `@task.pydantic`, which uses type hints in the definition of tasks to
+automatically (de)serialise arguments and returns.
---
+++ pydantic_provider/__init__.py
@@ -0,0 +1,90 @@
+from airflow.operators.python import PythonOperator
+from typing import Callable
+from pydantic import BaseModel
+import inspect
+from airflow.decorators.base import DecoratedOperator
+from airflow.decorators.base import task_decorator_factory
+from airflow.decorators.base import TaskDecorator
+from devtools import debug
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_provider_info():
+    return {
+        "package-name": "pydantic-provider",
+        "name": "airflow_provider_pydantic",
+        "description": "Automatic (de)serialisation",
+        "version": "0.1.0",
+        "task-decorators": [
+            {
+                "name": "pydantic",
+                "class-name": "pydantic_provider.pydantic_python_task",
+            }
+        ],
+    }
+
+
+class PydanticPythonOperator(PythonOperator):
+    def __init__(self, python_callable: Callable, *args, **kwargs):
+        super().__init__(python_callable=python_callable, *args, **kwargs)
+
+    def execute(self, context):
+        # Get the function arguments from the operator
+        args = self.op_args
+        kwargs = self.op_kwargs
+        debug(args, kwargs)
+
+        # Get the function signature
+        func_signature = inspect.signature(self.python_callable)
+
+        _args = [
+            self.deserialize_input(arg, param)
+            for arg, param in zip(args, func_signature.parameters.values())
+        ]
+        _kwargs = {
+            key: self.deserialize_input(value, func_signature.parameters[key])
+            for key, value in kwargs.items()
+        }
+
+        debug(args, kwargs, _args, _kwargs)
+
+        # Call the original Python callable
+        result = self.python_callable(*_args, **_kwargs)
+
+        # Serialize the result before returning it (if it's a Pydantic model)
+        return self.serialize_output(result)
+
+    def deserialize_input(self, input_value, parameter: inspect.Parameter):
+        """Deserialize input values, for example, Pydantic models."""
+        try:
+            return parameter.annotation.model_validate_json(input_value)
+        except AttributeError:
+            return input_value
+
+    def serialize_output(self, output_value):
+        """Serialize output values (e.g., convert Pydantic model back to dict)."""
+        try:
+            return output_value.model_dump_json()
+        except AttributeError:
+            return output_value
+
+
+class DecoratedPydanticPythonOperator(PydanticPythonOperator, DecoratedOperator):
+    custom_operator_name: str = "@task.pydantic"
+
+
+def pydantic_python_task(
+    python_callable: Callable | None = None,
+    multiple_outputs: bool | None = None,
+    **kwargs,
+) -> TaskDecorator:
+    return task_decorator_factory(
+        python_callable=python_callable,
+        multiple_outputs=multiple_outputs,
+        decorated_operator_class=PydanticPythonOperator,
+        **kwargs,
+    )
