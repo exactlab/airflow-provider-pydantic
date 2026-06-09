3073de2eaf51c113ff4bd6a22f49d46b58488217
cecizag <cecilia.zagni@exact-lab.it>
2026-06-09 12:43:19 +0200

    fixed importing from a deprecation shim

--- src/pydantic_provider/operators.py
+++ src/pydantic_provider/operators.py
@@ -2,7 +2,7 @@ import inspect
 import logging
 from typing import Callable
 
-from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk.bases.decorator import DecoratedOperator
 from airflow.sdk.bases.decorator import task_decorator_factory
 from airflow.sdk.bases.decorator import TaskDecorator
